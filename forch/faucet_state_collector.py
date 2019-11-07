2f5e72bb9bf4ae29e25966d564f3e3b19084538c
Yufeng Duan <55268016+didovesei@users.noreply.github.com>
2019-11-06 17:20:07 -0800

    change port state from active to up

--- forch/faucet_state_collector.py
+++ forch/faucet_state_collector.py
@@ -295,8 +295,8 @@ class FaucetStateCollector:
 
         # port dynamics
         if PORT_STATE_UP in port_states:
-            up = port_states[PORT_STATE_UP]
-            port_map[PORT_STATE] = constants.STATE_UP if up else constants.STATE_DOWN
+            port_up = port_states[PORT_STATE_UP]
+            port_map[PORT_STATE] = constants.STATE_UP if port_up else constants.STATE_DOWN
         else:
             port_map[PORT_STATE] = None
         port_map["state_last_change"] = port_states.get(PORT_STATE_TS)
