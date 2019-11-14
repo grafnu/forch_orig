9b85c7cab0fbc261d7e52fe41ac95a35f722261e
Yufeng Duan <55268016+didovesei@users.noreply.github.com>
2019-11-14 14:33:55 -0800

    handle empty host in http request

--- forch/http_server.py
+++ forch/http_server.py
@@ -27,12 +27,14 @@ class RequestHandler(http.server.BaseHTTPRequestHandler):
     # pylint: disable=invalid-name
     def do_GET(self):
         """Handle a basic http request get method"""
-        self.send_response(200)
-        self.end_headers()
         host = self.headers.get('Host')
         if not host:
+            self.send_response(500)
+            self.end_headers()
             LOGGER.warning("Host is empty. Path: %s", self.path)
             return
+        self.send_response(200)
+        self.end_headers()
         parsed = urllib.parse.urlparse(self.path)
         opts = {}
         opt_pairs = urllib.parse.parse_qsl(parsed.query)
