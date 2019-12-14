7cb76df29242b53b5ad634ab793d60378d8d7335
Trevor Pering <peringknife@google.com>
2019-12-13 16:47:35 -0800

    Rename to building_schema

--- forch/topology.py
+++ forch/topology.py
@@ -7,17 +7,17 @@ from forch.forchestrator import configure_logging
 
 from forch.utils import yaml_proto
 
-from forch.proto.device_spec_pb2 import DeviceSpec
+from forch.proto.building_schema_pb2 import BuildingSchema
 
 LOGGER = logging.getLogger('topology')
 
 def load_devices():
     """Load a device specification file"""
     base_dir_name = os.getenv('FORCH_CONFIG_DIR')
-    device_spec_file_name = os.path.join(base_dir_name, 'device_spec.yaml')
-    LOGGER.info('Loading device spec file %s', device_spec_file_name)
-    device_spec = yaml_proto(device_spec_file_name, DeviceSpec)
-    loaded_macs = list(device_spec.mac_addrs.keys())
+    building_schema_file_name = os.path.join(base_dir_name, 'building_schema.yaml')
+    LOGGER.info('Loading device spec file %s', building_schema_file_name)
+    building_schema = yaml_proto(building_schema_file_name, BuildingSchema)
+    loaded_macs = list(building_schema.mac_addrs.keys())
     loaded_macs.sort()
     LOGGER.info('Loaded device spec for devices: %s', loaded_macs)
     print(loaded_macs)
